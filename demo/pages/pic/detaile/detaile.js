c35be02b4015c587ed85f68cf1468de283d8055f
xushilong <xushilong@dyrs.com.cn>
2016-11-18 11:31:41 +0800

    update

---
+++ demo/pages/pic/detaile/detaile.js
@@ -0,0 +1,27 @@
+var app=getApp();
+Page({
+    data: {
+        data:null,
+        current:0,
+        indicatorDots: false,
+        autoplay: false,
+        duration:500
+    },
+    onReady:function(){
+        console.log(app.globalData.picData);
+        this.setData({
+            data:app.globalData.picData.data,
+            current:app.globalData.picData.current
+        })
+    },
+    onUnload:function(){
+        //生命周期结束清空数据
+        app.globalData.picData=null;
+    },
+    swiperChange: function(e) {
+        console.log(e.detail.current)
+        this.setData({
+            current:e.detail.current
+        })
+    }
+})
